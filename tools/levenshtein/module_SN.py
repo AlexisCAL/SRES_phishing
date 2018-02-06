d7fb94b37bef59c30c295724f606309947a9583a
Anon <anon@localhost>
2018-02-06 15:56:42 +0100

    Polish soundness test

--- tools/levenshtein/module_SN.py
+++ tools/levenshtein/module_SN.py
@@ -4,7 +4,8 @@ from PIL import Image
 from PIL import ImageDraw
 import pytesseract
 
-filename = 'images/tmp.jpg'
+filename = 'images/tmp'
+ext = '.jpg'
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract-ocr'
 
 # distance_against_known_list
@@ -27,25 +28,23 @@ def dakl(domain):
 def make_image(subdomain):
     size = width, height = 500, 30
     text = Image.new('RGB', size, (255,255,255))
-    font = ImageFont.truetype('DejaVuSansMono.ttf', 12)
+    font = ImageFont.truetype('DejaVuSerif.ttf', 16)
     draw = ImageDraw.Draw(text)
-    draw.text((5, 9), subdomain, font=font, fill=(0,0,0))
+    draw.text((5, 7), subdomain.upper(), font=font, fill=(0,0,0))
 
-    text.save(filename)
+    text.save(filename + ext)
 
 # Compare Levenshtein distance of OCR with domain string
 # Langues supportées: fra, eng, rus, chi_sim.
-def cowd(domain, language):
+def cowd(domain, language='eng'):
     subdomains = domain.split('.')
+    d = 0
     for subdomain in subdomains:
         if subdomain == 'www':
             continue
-        make_image(subdomain)
-        
-        ocr = pytesseract.image_to_string(Image.open(filename), lang=language, config='/usr/share/tessdata') # Changer la langue en fonction de IPAPI ?
+        make_image(subdomain.upper())
+        # Changer la langue en fonction de IPAPI ?
+        ocr = pytesseract.image_to_string(Image.open(filename + ext), config='/usr/share/tessdata', lang=language)
         print(ocr)
-        d = distance(subdomain, ocr)
-    
+        d = max(d, distance(subdomain.upper(), ocr))
     return d
-
-cowd('www.text.bonjour', 'eng')
